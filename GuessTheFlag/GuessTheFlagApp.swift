e302e571da6c7676f1f23ec780c80f0add4bbf5d
ayrton <ayrton.parkinson1@gmail.com>
2024-07-12 06:50:10 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ayrton Parkinson on 2024/07/12.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
